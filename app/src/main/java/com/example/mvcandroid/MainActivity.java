02221849f25d71941c107107066885f85c1d25da
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-17 22:25:07 +0300

    onCreate

--- app/src/main/java/com/example/mvcandroid/MainActivity.java
+++ app/src/main/java/com/example/mvcandroid/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.mvcandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Observer;
+
+public class MainActivity extends AppCompatActivity implements Observer, View.OnClickListener {
+    private Model mModel;
+    private Button btnCounter1;
+    private Button btnCounter2;
+    private Button btnCounter3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCounter1 = (Button) findViewById(R.id.btnCounter1);
+        btnCounter2 = (Button) findViewById(R.id.btnCounter2);
+        btnCounter3 = (Button) findViewById(R.id.btnCounter3);
+        btnCounter1.setOnClickListener(this);
+        btnCounter2.setOnClickListener(this);
+        btnCounter3.setOnClickListener(this);
+        mModel = new Model();
+        mModel.addObserver(this);
     }
+
 }
